a15953158acddc8bb71458bd67e135b7047f95b5
Nishil <nishilfoo@gmail.com>
2018-02-02 14:39:21 -0800

    dockerise mongo

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-docker run --rm -it -p 3000:3000 --name fg-volunteer-server charsi/fg-volunteer-server
+#!/bin/bash
+docker rm -f mongo
+docker run -v "$(pwd)/data":/data --name mongo -d mongo mongod --smallfiles
+docker rm -f fg-server
+docker run -d --name "fg-server" -t -i -v $PWD:/usr/src/app -p 3000:3000 --link mongo:mongo "charsi/fg-volunteer-server" 
+
+
